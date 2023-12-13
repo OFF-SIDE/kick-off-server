fe600483cdcfbae0136a5f54658b9d77571fe948
comeintovlanc <rkdtjdwns916@gmail.com>
2023-12-13 18:43:38 +0900

    [fix] timezone fixed

--- src/main/java/offside/server/OffSideServerApplication.java
+++ src/main/java/offside/server/OffSideServerApplication.java
@@ -1,5 +1,6 @@
 package offside.server;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class OffSideServerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(OffSideServerApplication.class, args);
 	}
 }
