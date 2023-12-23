675711c874b89c48dc0ce145fac0bb714823fd40
comeintovlanc <rkdtjdwns916@gmail.com>
2023-12-23 11:47:45 +0700

    [feat]: search and home page added

--- src/main/java/offside/server/stadium/controller/StadiumController.java
+++ src/main/java/offside/server/stadium/controller/StadiumController.java
@@ -2,7 +2,6 @@ package offside.server.stadium.controller;
 
 import jakarta.validation.Valid;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,10 +22,12 @@ import offside.server.util.service.UtilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class StadiumController {
     private final StadiumService stadiumService;
     private final UtilService utilService;
@@ -39,6 +40,26 @@ public class StadiumController {
         this.utilService = utilService;
         this.stadiumCrawlerService = stadiumCrawlerService;
         this.notificationService = notificationService;
+    }
+    @GetMapping("/home")
+    public String HomePage(Model model) {
+        
+        return "SearchHomePage";
+    }
+    
+    @GetMapping("/search")
+    public String StadiumSearchWeb(Model model, @RequestParam("location") String location) {
+        if (location.isEmpty() || location.equals("전체"))
+            location = "";
+        var tmpStadiumList = stadiumService.requestStadium(location,"");
+        if (tmpStadiumList.size() > 30)
+            tmpStadiumList = tmpStadiumList.subList(0,30);
+        
+        final var stadiumList = tmpStadiumList.stream().filter(stadium -> stadium.getExternalUrl().length() > 5).toList();
+        model.addAttribute("stadiumList", stadiumList);
+        model.addAttribute("location", location.equals("") ? "전체" : location);
+        model.addAttribute("outputCnt", stadiumList.size());
+        return "StadiumSearchPage";
     }
 
     // Stadium 등록 요청
--- src/test/java/offside/server/stadium/controller/StadiumControllerTest.java
+++ src/test/java/offside/server/stadium/controller/StadiumControllerTest.java
@@ -26,5 +26,24 @@ class StadiumControllerTest {
     }
 
     @Test
-    void 구장_해당날짜_상세보기() {}
+    void 구장_등록하기() {
+        final var stadium1 = new StadiumDto("마포구","0231539874","서울특별시 산악문화체험센터>난지천인조잔디축구장","서울시 마포구 신수동 12-32","1. 공공시설 예약서비스 이용시 필수 준수사항모든 서비스의 이용은 담당 기관의 규정에 따릅니다. 각 시설의 규정 및 허가조건을 반드시 준수하여야 합니다",50000,"https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=1617239932085QQ2HH7Q6BFUUKKLU129S2081E","");
+        final var stadium2 = new StadiumDto("마포구","0231539875","2023년 농구장1-망원한강공원","망원한강공원>농구장(1,2)","1. 공공시설 예약서비스 이용시 필수 준수사항모든 서비스의 이용은 담당 기관의 규정에 따릅니다. 각 시설의 규정 및 허가조건을 반드시 준수하여야 합니다",30000,"https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=16726384080191167IF8ED1YE52CJTBDVFA92K","");
+        final var stadium3 = new StadiumDto("영등포구","0231539876","2024년 배구장2-양화한강공원","양화한강공원>배구장(1,2)","1. 공공시설 예약서비스 이용시 필수 준수사항모든 서비스의 이용은 담당 기관의 규정에 따릅니다. 각 시설의 규정 및 허가조건을 반드시 준수하여야 합니다",20000,"https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S231102162929228428","");
+        
+        final var newStadium1 = stadiumService.registerStadium(stadium1);
+        final var newStadium2 = stadiumService.registerStadium(stadium2);
+        final var newStadium3 = stadiumService.registerStadium(stadium3);
+        
+        final var stadiumInfo1 = stadiumService.getStadiumInfo(newStadium1.getId());
+        final var stadiumInfo2 = stadiumService.getStadiumInfo(newStadium2.getId());
+        final var stadiumInfo3 = stadiumService.getStadiumInfo(newStadium3.getId());
+        
+        assertThat(stadium1.name).isEqualTo(stadiumInfo1.getName());
+        assertThat(stadium2.address).isEqualTo(newStadium2.getAddress());
+        assertThat(stadium3.image).isEqualTo(newStadium3.getImage());
+        assertThat(stadium1.price).isEqualTo(newStadium1.getPrice());
+        assertThat(stadium2.externalUrl).isEqualTo(stadiumInfo2.getExternalUrl());
+        assertThat(stadium3.name).isEqualTo(stadiumInfo3.getName());
+    }
 }
